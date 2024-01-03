2ff456e2f70ad3122f6dc582aac69e5836de2b5b
Kappamaki64 <takakun.0907050301@gmail.com>
2024-01-03 19:33:26 +0900

    feat: add base code

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    public static void main(String[] args) {
+        System.out.println("extracted code");
+
+        String renamedVariable1 = "renamed variable";
+        System.out.println(renamedVariable1);
+
+        String inlinedVariable = "inlined code";
+        System.out.println(inlinedVariable);
+    }
+}
