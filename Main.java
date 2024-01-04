e3b144d58a58a6a2f16a8758b2f1069e4eb51d65
Kappamaki64 <takakun.0907050301@gmail.com>
2024-01-05 00:46:30 +0900

    feat: add base code

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public void method1(int a) {
+        if (a == 0) {
+            return;
+        }
+
+        if (a != 1) {
+            System.out.println("No");
+        } else {
+            System.out.println("Yes");
+        }
+    }
+}
