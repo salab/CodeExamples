530b045223783a30ba3afbf659df7b234c117af5
Kappamaki64 <takakun.0907050301@gmail.com>
2024-01-05 00:53:09 +0900

    refactor: composite local refactoring

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public void method1(int a) {
-        if (a == 0) {
-            return;
-        }
-
-        if (a != 1) {
-            System.out.println("No");
-        } else {
-            System.out.println("Yes");
+        switch (a) {
+            case 0:
+                return;
+            case 1:
+                System.out.println("Yes");
+                break;
+            default:
+                System.out.println("No");
+                break;
         }
     }
 }
