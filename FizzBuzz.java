c9a753c73fff60c56543d920f3f021b5bc3bd982
Kappamaki64 <takakun.0907050301@gmail.com>
2024-01-07 19:15:09 +0900

    refactor: extract get() method

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,12 +1,17 @@
 public class FizzBuzz {
     public void print(int maxCount) {
         for (int count = 1; count <= maxCount; count++) {
-            String s = "" + count;
-            if (count % 3 == 0) s = "Fizz";
-            if (count % 5 == 0) s = "Buzz";
-            if (count % 15 == 0) s = "FizzBuzz";
+            String s = get(count);
             System.out.println(s);
         }
+    }
+
+    private String get(int count) {
+        String s = "" + count;
+        if (count % 3 == 0) s = "Fizz";
+        if (count % 5 == 0) s = "Buzz";
+        if (count % 15 == 0) s = "FizzBuzz";
+        return s;
     }
 
     public static void main(String[] args) {
